3dbcb3d79ab93e2e75844f1cff30dc667571d5d2
Aditya Reddy Ramannagri <47287316+adireddyr97@users.noreply.github.com>
2020-04-12 10:55:46 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,28 @@
 import numpy as np
+from numpy.linalg import inv
+
+def pyramid(flo):   
+  flow_level = []
+  velocity = []
+  
+  for i in range(flo)-1:
+    
+    flow = np.subtract(flo[i+1],flo[i])
+    flow_level.append(flow)
+  
+  flow_level.append(flo[2])
+  
+  for i in range(flow_level)-1:
+    
+    vel = np.subtract(flow_level[i+1],flow_level[i])
+    velocity.append(vel)
+   
+  acceleration = np.subtract(velocity[1],velocity[0])
+  
+  velocity.append(np.add(velcoity[1],acceleration))
+  
+  flow_level.append(np.add(velocity[2],flow_level[2]))
+  
+  inverse_flow = inv(flow_level[3])
+  
+  return np.array(inverse_flow)
